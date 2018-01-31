43df14ad2831cc8e6273bb50572a329153c4bb11
Desentso <olli.loukojarvi@hotmail.com>
2018-01-31 10:36:17 +0200

    Dataflow done, chart still WIP

--- public/index.js
+++ public/index.js
@@ -40,7 +40,7 @@ class Day extends React.Component {
 		return (
 			<div className="Day" onClick={this.handleClick}>
 				<h2 className="dayTitle">{this.props.data.day}</h2>
-				<img className="icon" src={this.props.data.icon} />
+				<img className="icon" src={this.props.data.weatherIconUrl} />
 				<p className="temp">{this.props.data.temperature} &deg;C</p>
 				<p className="maxTemp">Max: {this.props.data.maxTemperature} &deg;C</p>
 				<p className="minTemp">Min: {this.props.data.minTemperature} &deg;C</p>
@@ -55,17 +55,43 @@ class Chart extends React.Component {
 		super(props);
 	}
 
+	componentDidMount() {
+        this.initializeChart();
+    }
+
+    initializeChart() {
+        const elem = ReactDOM.findDOMNode(this.refs.chart);
+        const ctx = elem.getContext("2d");
+
+        const data = [];
+        const propsData = this.props.data.forecast;
+        Object.keys(propsData).forEach((key, index) => {
+        	data.push({x: parseInt(key), y: propsData[key].temperature});
+        });
+
+        const config = {
+        	type: "line",
+        	data: data,
+        	options: {}
+        };
+
+        const chart = new Chart(ctx, {type: "line", data: data});
+
+        console.log(this.refs.chart);
+        console.log(chart);
+    }
+
 	render(){
 		return (
 			<div className="Chart">
-				<Day data={this.props.data} />
+				<canvas id="chart" ref="chart" height="250px" width="350px" />
 			</div>
 		)
 	}	
 };
 
 
-const DATA = [
+/*const DATA = [
 	{day: "Monday", temperature: 15, maxTemperature: 18, minTemperature: 14, icon: "#"},
 	{day: "Tuesday", temperature: 15, maxTemperature: 18, minTemperature: 14, icon: "#"},
 	{day: "Wednesday", temperature: 15, maxTemperature: 18, minTemperature: 14, icon: "#"},
@@ -73,9 +99,35 @@ const DATA = [
 	{day: "Friday", temperature: 15, maxTemperature: 18, minTemperature: 14, icon: "#"},
 	{day: "Saturday", temperature: 15, maxTemperature: 18, minTemperature: 14, icon: "#"},
 	{day: "Sunday", temperature: 15, maxTemperature: 18, minTemperature: 14, icon: "#"},
-];
+];*/
+const id = "123";
+fetch("/forecast/" + id).then(resp => resp.json())
+.then(data => {
 
-ReactDOM.render(
+	const DATA = [];
+	Object.keys(data).forEach((key, index) => {
+		const newObj = data[key];
+		newObj.day = getWeekDay(key);
+		DATA.push(newObj);
+	});
+
+	ReactDOM.render(
+		<App data={DATA} />,
+		document.getElementById("app")
+	);
+})
+.catch(error => {
+	console.log(error);
+})
+
+/*ReactDOM.render(
 	<App data={DATA} />,
 	document.getElementById("app")
-);
+);*/
+
+function getWeekDay(dateStr) {
+	const date = new Date(dateStr);
+	const weekdayNo = date.getDay();
+	const weekdays = {0: "Sunday", 1: "Monday", 2: "Tuesday", 3: "Wednesday", 4: "Thursday", 5: "Friday", 6: "Saturday"};
+	return weekdays[weekdayNo];
+};
--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const http = require("http");
 const path = require("path");
 const bodyParser = require("body-parser");
-/*const axios = require("axios");
-const async = require("async");*/
+const axios = require("axios");
+const async = require("async");
 
 const app = express();
 
@@ -20,3 +20,74 @@ app.get("/", (req, resp) => {
 
 	resp.sendFile(path.join(__dirname + "/public/index.html"));
 });
+
+app.get("/forecast/:id", (req, resp) => {
+	//temperature: 15, maxTemperature: 18, minTemperature: 14, icon: "#"},
+
+	const data = {};
+	const days = {};
+	axios.get("http://api.openweathermap.org/data/2.5/forecast?id=658226&appid=433d4e8a404bcfc906fd28bed3a75f60")
+	.then(resp2 => {
+		const forecastList = resp2.data.list;
+		async.each(forecastList, (forecast, callback) => {
+
+			const day = forecast.dt_txt.slice(0,10);
+			const hour = forecast.dt_txt.slice(11,13);
+
+			if (days.hasOwnProperty(day)) {
+
+				const maxTemp = toCelsius(forecast.main.temp_max)
+				const minTemp = toCelsius(forecast.main.temp_min)
+				const temp = toCelsius(forecast.main.temp)
+
+				days[day].forecast[hour] = {
+						temperature: temp, 
+						maxTemperature: maxTemp,
+						minTemperature: minTemp,
+						weatherDescription: forecast.weather[0].description,
+						weatherIconUrl: "http://openweathermap.org/img/w/" + forecast.weather[0].icon + ".png"
+					}
+				days[day].temps = days[day].temps.concat([temp, maxTemp, minTemp]);
+			} else {
+				//data[day] = {};
+				days[day] = {temps: [], forecast: {}};
+			}
+
+			callback(null);
+		}, () => {
+
+			async.each(Object.keys(days), (day, callback2) => {
+
+				let middle = Math.floor((Object.keys(days[day].forecast).length) / 2);
+				console.log(middle);
+				middle = Object.keys(days[day].forecast).sort((a,b)=>parseInt(a) > parseInt(b))[middle];
+				console.log(Object.keys(days[day].forecast).sort((a,b)=>parseInt(a) > parseInt(b)));
+				console.log(middle);
+				days[day].maxTemperature = Math.max.apply(null, days[day].temps);
+				days[day].minTemperature = Math.min.apply(null, days[day].temps);
+				days[day].temperature = days[day].forecast[middle].temperature;
+				days[day].weatherDescription = days[day].forecast[middle].weatherDescription;
+				days[day].weatherIconUrl = days[day].forecast[middle].weatherIconUrl;
+
+				callback2(null);
+			}, () => {
+
+				const DATA = days;//{forecastByDayAndHour: data, forecastByDay: days};
+
+				resp.setHeader('Content-Type', 'application/json');
+				resp.send(JSON.stringify(DATA));
+			});
+
+			/*resp.setHeader('Content-Type', 'application/json');
+			resp.send(JSON.stringify(data));*/
+		});
+	})
+	.catch(error => {
+		console.log(error);
+		resp.sendStatus(400);
+	})
+});
+
+function toCelsius(temp) {
+	return Math.round((temp - 273.15)*2)/2;
+};
